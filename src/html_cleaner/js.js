e4ea4bffccfe0a18d72489f8326b22b3b0c19854
Glutanimate <glutanimate@users.noreply.github.com>
2020-06-25 17:31:59 +0200

    Run 'prettier'

--- src/html_cleaner/js.js
+++ src/html_cleaner/js.js
@@ -1,22 +1,21 @@
 function onClean(idx) {
-    cmd = "clean:" + idx;
-    pycmd(cmd);
+  cmd = "clean:" + idx;
+  pycmd(cmd);
 }
 
-
 function setCleanFields() {
-    $fnames = $(".fname");
-    for (var i=0; i<$fnames.length; i++) {
-        var $td_name = $(`#name${i}`);
-        var div = `, <a onclick='onClean(${i})'>clean</a>`;
-        $td_name.append(div);
-    }
+  $fnames = $(".fname");
+  for (var i = 0; i < $fnames.length; i++) {
+    var $td_name = $(`#name${i}`);
+    var div = `, <a onclick='onClean(${i})'>clean</a>`;
+    $td_name.append(div);
+  }
 }
 
 function makeNonContentEditable() {
-    console.log('in makeNonContentEditable')
-    var allfields = document.getElementsByClassName("field clearfix");
-    for (var i = 0; i < allfields.length; i++) {
-        $(allfields.item(i)).prop('contenteditable', false);
-     }
+  console.log("in makeNonContentEditable");
+  var allfields = document.getElementsByClassName("field clearfix");
+  for (var i = 0; i < allfields.length; i++) {
+    $(allfields.item(i)).prop("contenteditable", false);
+  }
 }
